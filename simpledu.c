18aa5bd8addf6ef0a1ddbe1a2d8a732fbde649aa
andrenasx <andre2000.an@gmail.com>
2020-03-29 15:02:29 +0100

    Init

---
+++ simpledu.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[], char *envp[]){
+    if(argc < 3){
+        printf("Usage: %s -l [path] [-a] [-b] [-B size] [-L] [-S] [--max-depth=N]\n", argv[0]);
+        exit(1);
+    }
+
+    return 0;
+}
