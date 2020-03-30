1b5ec037753ff6dadc24aaf1dc26c4c0d6b80369
andrenasx <andre2000.an@gmail.com>
2020-03-30 12:23:26 +0100

    added header

---
+++ simpledu.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
+
+#include "args.h"
